00267e7043572d35d22e4cb1d837344707582749
EZH1809 <stellage.ua@gmail.com>
2024-04-17 20:37:30 +0300

    add

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -10,10 +10,9 @@ const UserSchema = Yup.object().shape({
     .max(50, 'Too Long!')
     .required('Required'),
   number: Yup.string()
-    .min(3, 'Too Short!')
-    .max(50, 'Too Long!')
+    .matches(/^\d{3}-\d{2}-\d{2}$/, 'Invalid phone number format')
     .required('Required'),
-});
+}); 
 
 const initialValues = {
   name: '',
